e40aefb18125a65ea5d60d9a9b45862bc5fcc5c3
Nicholas Barella <soonablabs@gmail.com>
2025-10-10 14:54:44 +0100

    db: add SetupCheck table

---
+++ prisma/migrations/20251010135439_setupcheck_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "SetupCheck" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "SetupCheck_pkey" PRIMARY KEY ("id")
+);
